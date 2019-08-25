976962a3c1e06050d9a40e77b8e809ebef85e974
Matlockyholmes <matthiasheylen1@gmail.com>
2019-08-25 22:13:28 +0200

    Pretty aight version + met birthyear post 2000 vali

--- src/main/java/be/vdab/personeel/constraints/RijksregisternummerValidator.java
+++ src/main/java/be/vdab/personeel/constraints/RijksregisternummerValidator.java
@@ -23,7 +23,9 @@ public class RijksregisternummerValidator implements ConstraintValidator<Rijksre
         boolean correctMaand = geboorteDatum.getMonthValue() == ((rijksregisternummer / 10000000) % 100);
         boolean correctDag = geboorteDatum.getDayOfMonth() == ((rijksregisternummer / 100000) % 100);
 
-        if(correctDag && correctMaand && correctJaar){
+        if(correctDag && correctMaand && correctJaar && (geboorteDatum.getYear()) > 2000){
+            return controlegetal == 97 - (2000000000 * (rijksregisternummer / 100) % 97);
+        } else if(correctDag && correctMaand && correctJaar){
             return controlegetal == 97 - ((rijksregisternummer / 100) % 97);
         }
         return false;
