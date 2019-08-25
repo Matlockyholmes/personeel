2c142e1bb3bcea3dfa4cc2d85ebe27dceed796ac
Matlockyholmes <matthiasheylen1@gmail.com>
2019-08-25 13:09:52 +0200

    Eureka!

--- src/main/java/be/vdab/personeel/controllers/WerknemerController.java
+++ src/main/java/be/vdab/personeel/controllers/WerknemerController.java
@@ -4,22 +4,21 @@ import be.vdab.personeel.domain.Werknemer;
 import be.vdab.personeel.forms.OpslagForm;
 import be.vdab.personeel.services.WerknemerService;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Controller
 @RequestMapping("werknemershierarchie")
 public class WerknemerController {
     private final WerknemerService werknemerService;
-    private static final String REDIRECT_NA_SUCCES_OPSLAG = "redirect:/werknemershierarchie/{id}";
+    private static final String REDIRECT_NA_SUCCES = "redirect:/werknemershierarchie/{id}";
     private static final String REDIRECT_NA_FAAL_OPSLAG = "redirect:/werknemershierarchie/opslag/{id}";
 
     public WerknemerController(WerknemerService werknemerService) {
@@ -39,23 +38,30 @@ public class WerknemerController {
     }
 
     @GetMapping("opslag/{optionalWerknemer}")
-    public ModelAndView toonOpslag(@PathVariable Optional<Werknemer> optionalWerknemer){
-        ModelAndView modelAndView = new ModelAndView("opslag").addObject(new OpslagForm(null));
+    public ModelAndView toonOpslag(@PathVariable Optional<Werknemer> optionalWerknemer, Model model){
+        ModelAndView modelAndView = new ModelAndView("opslag");
+        if (!model.containsAttribute("opslagForm")){
+            model.addAttribute("opslagForm", new OpslagForm(null));
+            modelAndView.addObject(new OpslagForm(null));
+        }
         optionalWerknemer.ifPresent(werknemer -> modelAndView.addObject(werknemer));
         return modelAndView;
     }
 
     @PostMapping("opslag/{optionalWerknemer}")
-    public ModelAndView geefOpslag(@PathVariable Optional<Werknemer> optionalWerknemer, @Valid OpslagForm opslagForm, Errors errors, RedirectAttributes redirectAttributes){
-       // redirectAttributes.addAttribute("id", optionalWerknemer.get().getId());
-        ModelAndView modelAndView = new ModelAndView("opslag");
-        if(!errors.hasErrors()){
-            optionalWerknemer.ifPresent(werknemer -> werknemerService.saveOpslag(werknemer, opslagForm.getOpslag()));
-           // return REDIRECT_NA_SUCCES_OPSLAG;
+    public String geefOpslag(@PathVariable Optional<Werknemer> optionalWerknemer, @Valid @ModelAttribute("opslagForm") OpslagForm opslagForm,
+                             BindingResult binding, RedirectAttributes redirectAttributes){
+        AtomicLong id = new AtomicLong();
+        optionalWerknemer.ifPresent(werknemer -> id.set(werknemer.getId()));
+        redirectAttributes.addAttribute("id", id);
+        if (binding.hasErrors()) {
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.opslagForm", binding);
+            redirectAttributes.addFlashAttribute("opslagForm", opslagForm);
+            return REDIRECT_NA_FAAL_OPSLAG;
         }
-        optionalWerknemer.ifPresent(werknemer -> modelAndView.addObject(werknemer));
-        return modelAndView;
-       // return REDIRECT_NA_FAAL_OPSLAG;
+        optionalWerknemer.ifPresent(werknemer -> werknemerService.saveOpslag(werknemer, opslagForm.getOpslag()));
+        return REDIRECT_NA_SUCCES;
     }
+
 
 }
